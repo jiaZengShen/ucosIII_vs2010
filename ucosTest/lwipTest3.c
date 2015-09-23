5f75a9bf78abba07272e23748e6f1428f00b4b9e
jeanWin <501821782@qq.com>
2015-09-23 17:47:28 +0800

    ucos与lwip整合

--- ucosTest/lwipTest3.c
+++ ucosTest/lwipTest3.c
@@ -21,7 +21,7 @@
 #include "lwip/dns.h"
 #include "lwip/dhcp.h"
 #include "lwip/autoip.h"
-
+#include <includes.h>
 /* lwIP netif includes */
 #include "netif/etharp.h"
 
@@ -136,30 +136,16 @@ static void	msvc_netif_init()
 {
 	ip_addr_t ipaddr, netmask, gw;
 
-
-
-	
-
-
 	ip_addr_set_zero(&gw);
 	ip_addr_set_zero(&ipaddr);
 	ip_addr_set_zero(&netmask);
-
-
 	LWIP_PORT_INIT_GW(&gw);//设置网关
 	LWIP_PORT_INIT_IPADDR(&ipaddr);//设置ip地址
 	LWIP_PORT_INIT_NETMASK(&netmask);//设置掩码
 	printf("Starting lwIP, local interface IP is %s\n", ip_ntoa(&ipaddr));
 	printf("the 网关是%s\n",ip_ntoa(&gw) );
-
-
-
 	netif_set_default(netif_add(&netif, &ipaddr, &netmask, &gw, NULL, pcapif_init, tcpip_input));
-
-
 	netif_set_status_callback(&netif, status_callback);
-
-
 	netif_set_link_callback(&netif, link_callback);
 	netif_set_up(&netif);
 }
@@ -205,7 +191,7 @@ static void	apps_init()
  * the main_loop context (there is no other one), when NO_SYS=0, this is done
  * in the tcpip_thread context */
 static void
-test_init(void * arg)
+myTcp_init(void * arg)
 { /* remove compiler warning */
 
   sys_sem_t *init_sem;
@@ -222,7 +208,172 @@ test_init(void * arg)
   sys_sem_signal(init_sem);
 
 }
+static  OS_TCB   AppTaskStartTCB;
+static  CPU_STK  AppTaskStartStk[APP_TASK_START_STK_SIZE];
 
+static  OS_TCB   ATCB;
+static  CPU_STK  ATaskStartStk[APP_TASK_START_STK_SIZE];
+
+static  OS_TCB   BTCB;
+static  CPU_STK  BTaskStartStk[APP_TASK_START_STK_SIZE];
+
+#include "os_app_hooks.h"
+
+OS_SEM *p_sem ;//指向信号量
+OS_SEM g_sem ;//实际的信号量
+void  MyTaskA (void *p_arg)
+{
+	OS_ERR  err;
+	APP_TRACE_DBG(("A is Running...\n\r"));
+	while (DEF_TRUE) {        
+		//如果把延时去掉，那么就不会实现任务切换了。
+		/* Task body, always written as an infinite loop.       */
+		OSTimeDlyHMSM(0, 0, 1, 0,
+			OS_OPT_TIME_DLY,
+			&err);
+		//使用信号量来决定，谁能输出
+		OSSemPend(p_sem,0,OS_OPT_PEND_BLOCKING,NULL,&err);
+		if(err == OS_ERR_NONE)//没有出错
+		{
+			APP_TRACE_DBG(("A: %d\n\r", OSTimeGet(&err)));
+		}
+		OSSemPost(p_sem,OS_OPT_POST_ALL,&err);
+	}
+}
+
+void MyTaskB(void *p_arg)
+{
+	OS_ERR  err;
+	APP_TRACE_DBG(("B is Running...\n\r"));
+	while (DEF_TRUE) {                                            /* Task body, always written as an infinite loop.       */
+		OSTimeDlyHMSM(0, 0, 1, 0,
+			OS_OPT_TIME_DLY,
+			&err);
+		//使用信号量来决定，谁能输出
+		OSSemPend(p_sem,0,OS_OPT_PEND_BLOCKING,NULL,&err);
+		if(err == OS_ERR_NONE)//没有出错
+		{
+			APP_TRACE_DBG(("B: %d\n\r", OSTimeGet(&err)));
+		}
+		OSSemPost(p_sem,OS_OPT_POST_ALL,&err);
+	}
+}
+static  void  AppTaskStart (void *p_arg)
+{
+    OS_ERR  err;
+
+
+   (void)p_arg;
+
+    BSP_Init();                                                 /* Initialize BSP functions                             */
+    CPU_Init();                                                 /* Initialize uC/CPU services                           */
+
+#if OS_CFG_STAT_TASK_EN > 0u
+    OSStatTaskCPUUsageInit(&err);                               /* Compute CPU capacity with no task running            */
+#endif
+#if OS_CFG_APP_HOOKS_EN				//使用钩子函数
+	App_OS_SetAllHooks();			
+#endif
+    APP_TRACE_DBG(("uCOS-III is Running...\n\r"));
+	OS_CRITICAL_ENTER();	//进入临界区
+	p_sem = &g_sem;
+	//初始化一些参数
+	//tcpip_init(NULL,NULL);
+	//创建信号量
+	OSSemCreate(p_sem,
+		"sem_name",
+		1,
+		&err);
+	//创建任务
+	//A
+	OSTaskCreate((OS_TCB     *)&ATCB,                /* Create the start task                                */
+		(CPU_CHAR   *)"A",
+		(OS_TASK_PTR ) MyTaskA,
+		(void       *) 0,
+		(OS_PRIO     ) APP_TASK_START_PRIO+1,
+		(CPU_STK    *)&ATaskStartStk[0],
+		(CPU_STK_SIZE) APP_TASK_START_STK_SIZE / 10u,
+		(CPU_STK_SIZE) APP_TASK_START_STK_SIZE,
+		(OS_MSG_QTY  ) 0u,
+		(OS_TICK     ) 0u,
+		(void       *) 0,
+		(OS_OPT      )(OS_OPT_TASK_STK_CHK | OS_OPT_TASK_STK_CLR),
+		(OS_ERR     *)&err);
+	////B
+	OSTaskCreate((OS_TCB     *)&BTCB,                /* Create the start task                                */
+		(CPU_CHAR   *)"A",
+		(OS_TASK_PTR ) MyTaskB,
+		(void       *) 0,
+		(OS_PRIO     ) APP_TASK_START_PRIO+2,//优先级必须相互区分
+		(CPU_STK    *)&BTaskStartStk[0],
+		(CPU_STK_SIZE) APP_TASK_START_STK_SIZE / 10u,
+		(CPU_STK_SIZE) APP_TASK_START_STK_SIZE,
+		(OS_MSG_QTY  ) 0u,
+		(OS_TICK     ) 0u,
+		(void       *) 0,
+		(OS_OPT      )(OS_OPT_TASK_STK_CHK | OS_OPT_TASK_STK_CLR),
+		(OS_ERR     *)&err);
+	////创建ping的test
+	//OSTaskCreate((OS_TCB     *)&pingTCB,                /* Create the start task                                */
+	//	(CPU_CHAR   *)"ping",
+	//	(OS_TASK_PTR ) pingTestTask,
+	//	(void       *) 0,
+	//	(OS_PRIO     ) APP_TASK_START_PRIO+3,
+	//	(CPU_STK    *)&pingTaskStartStk[0],
+	//	(CPU_STK_SIZE) APP_TASK_START_STK_SIZE / 10u,
+	//	(CPU_STK_SIZE) APP_TASK_START_STK_SIZE,
+	//	(OS_MSG_QTY  ) 0u,
+	//	(OS_TICK     ) 0u,
+	//	(void       *) 0,
+	//	(OS_OPT      )(OS_OPT_TASK_STK_CHK | OS_OPT_TASK_STK_CLR),
+	//	(OS_ERR     *)&err);
+	//client测试程序
+	//OSTaskCreate((OS_TCB     *)&clientTCB,                /* Create the start task                                */
+	//	(CPU_CHAR   *)"client",
+	//	(OS_TASK_PTR ) clientTestTask,
+	//	(void       *) 0,
+	//	(OS_PRIO     ) APP_TASK_START_PRIO+4,
+	//	(CPU_STK    *)&clientTaskStartStk[0],
+	//	(CPU_STK_SIZE) APP_TASK_START_STK_SIZE / 10u,
+	//	(CPU_STK_SIZE) APP_TASK_START_STK_SIZE,
+	//	(OS_MSG_QTY  ) 0u,
+	//	(OS_TICK     ) 0u,
+	//	(void       *) 0,
+	//	(OS_OPT      )(OS_OPT_TASK_STK_CHK | OS_OPT_TASK_STK_CLR),
+	//	(OS_ERR     *)&err);
+	OS_CRITICAL_EXIT();	//退出临界区
+
+	//OSTaskDel((OS_TCB*)0,&err);	//删除start_task任务自身
+
+	//lwip_init();
+	
+	//ping_init();
+    while (DEF_ON) {                                            /* Task body, always written as an infinite loop.       */
+		OSTimeDlyHMSM(0, 0, 1, 0,
+			OS_OPT_TIME_DLY,
+			&err);
+
+        APP_TRACE_DBG(("Time: %d\n\r", OSTimeGet(&err)));
+    }
+}
+void osInit()
+{
+	OS_ERR  err;
+	OSInit(&err);                                               /* Init uC/OS-III.                                      */
+	OSTaskCreate((OS_TCB     *)&AppTaskStartTCB,                /* Create the start task                                */
+	                (CPU_CHAR   *)"App Task Start",
+	                (OS_TASK_PTR ) AppTaskStart,
+	                (void       *) 0,
+	                (OS_PRIO     ) APP_TASK_START_PRIO,
+	                (CPU_STK    *)&AppTaskStartStk[0],
+	                (CPU_STK_SIZE) APP_TASK_START_STK_SIZE / 10u,
+	                (CPU_STK_SIZE) APP_TASK_START_STK_SIZE,
+	                (OS_MSG_QTY  ) 0u,
+	                (OS_TICK     ) 0u,
+	                (void       *) 0,
+	                (OS_OPT      )(OS_OPT_TASK_STK_CHK | OS_OPT_TASK_STK_CLR),
+	                (OS_ERR     *)&err);
+}
 
 /* This is somewhat different to other ports: we have a main loop here:
  * a dedicated task that waits for packets to arrive. This would normally be
@@ -239,7 +390,7 @@ void main_loop()
   /* initialize lwIP stack, network interfaces and applications */
 
   err = sys_sem_new(&init_sem, 0);
-  tcpip_init(test_init, &init_sem);
+  tcpip_init(myTcp_init, &init_sem);
   /* we have to wait for initialization to finish before
    * calling update_adapter()! */
   sys_sem_wait(&init_sem);
@@ -247,18 +398,11 @@ void main_loop()
 
 
   /* MAIN LOOP for driver update (and timers if NO_SYS) */
+  osInit();
+  OSStart(&err); 
   while (!_kbhit()) {
-
-
-
-
     sys_msleep(50);
-
-
-
   }
-
-
 
   /* release the pcap library... */
   pcapif_shutdown(&netif);
@@ -268,11 +412,8 @@ void main_loop()
 void main(void)
 
 {
-
 	/* no stdio-buffering, please! */
 	setvbuf(stdout, NULL,_IONBF, 0);
-
 	main_loop();
-
 	return ;
 }
